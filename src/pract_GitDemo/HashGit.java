81d5d68b5c287958853059072c1847bb3210e377
Akshayv18 <akshayvkamble93@gmail.com>
2026-01-31 00:26:44 +0530

    Demo project

---
+++ src/pract_GitDemo/HashGit.java
@@ -0,0 +1,17 @@
+package pract_GitDemo;
+
+import java.util.HashSet;
+
+public class HashGit {
+
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<String>();
+        set.add("Akshay");
+        set.add("Java");
+        set.add("Developer");
+        for(String s:set) {
+        	System.out.println(s);
+        }
+	}
+
+}
---
+++ src/pract_GitDemo/HashmapGit.java
@@ -0,0 +1,10 @@
+package pract_GitDemo;
+
+public class HashmapGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
